f4eaff2ca1173bc284db0248863c20180fd2df6f
Hendrik Mans <hendrik@mans.de>
2018-03-31 20:49:19 +0200

    Use slang instead of ecr

--- src/crankypants/web.cr
+++ src/crankypants/web.cr
@@ -1,16 +1,15 @@
 require "kemal"
+require "kilt/slang"
 
 get "/" do
   posts = Crankypants::Repo.all(Crankypants::Post)
-
-  render "src/views/index.ecr"
+  render "src/views/index.slang"
 end
 
 get "/posts/:id" do |env|
   id = env.params.url["id"]
   post = Crankypants::Repo.get!(Crankypants::Post, id)
-
-  render "src/views/post.ecr"
+  render "src/views/post.slang"
 end
 
 module Crankypants
