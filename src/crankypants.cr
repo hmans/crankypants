39f6c42d586a30c49baaaef52deaacf7af07839e
Hendrik Mans <hendrik@mans.de>
2018-04-01 10:38:13 +0200

    Cleanup

--- src/crankypants.cr
+++ src/crankypants.cr
@@ -1,21 +1,13 @@
-require "./crankypants/migrator"
-require "./crankypants/repo"
 require "./crankypants/*"
 
 module Crankypants
 end
 
+# Create and/or migrate our database to the latest
+# schema version.
+#
 Migrator.new(url: "sqlite3://./data.db").run_pending_migrations!
 
-# -- +micrate Up
-# -- SQL in section 'Up' is executed when this migration is applied
-# create table posts (id int primary key, key text, title text, body text);
-# insert into posts values (1, "hello-world", "Hello world", "I am the first post. Isn't it amazing?");
+# Run our web app.
 #
-# -- +micrate Down
-# -- SQL section 'Down' is executed when this migration is rolled back
-# drop table posts;
-
-
-# Run web app
 Crankypants::Web.run
--- src/crankypants/web.cr
+++ src/crankypants/web.cr
@@ -11,42 +11,49 @@ macro render_page(filename)
   render "src/views/#{{{filename}}}.slang", "src/views/layouts/application.slang"
 end
 
-get "/" do
-  posts = Repo.all(Post, Query.order_by("created_at DESC"))
-  render_page "posts/index"
-end
-
-get "/crankypants.js" do |env|
-  env.response.headers.add "Cache-Control", "max-age=600, public"
-  env.response.content_type = "text/javascript"
-  render "dist/bundle.js.ecr"
-end
-
-# get "/crankypants.css" do |env|
-#   env.response.content_type = "text/css"
-#   render "src/views/crankypants.css.ecr"
-# end
-
-post "/posts" do |env|
-  post = Post.new
-  post.title = env.params.body["post[title]"].as(String)
-  post.body =  env.params.body["post[body]"].as(String)
-
-  _changeset = Repo.insert(post)
-
-  env.redirect "/"
-end
-
-get "/posts/:id" do |env|
-  post = Repo.get!(Post, env.params.url["id"])
-  render_page "posts/show"
-end
-
 module Crankypants
   module Web
-    extend self
+    def self.run
+      # Our root page renders this site's latest posts.
+      #
+      get "/" do
+        posts = Repo.all(Post, Query.order_by("created_at DESC"))
+        render_page "posts/index"
+      end
 
-    def run
+      # This is where we render individual posts.
+      #
+      get "/posts/:id" do |env|
+        post = Repo.get!(Post, env.params.url["id"])
+        render_page "posts/show"
+      end
+
+      # We compile our Webpack bundle into this app to serve
+      # it directly. Yes, this is sick and twisted, but the goal
+      # is to have a single self-contained executable, so there
+      # you go.
+      #
+      get "/crankypants.js" do |env|
+        env.response.headers.add "Cache-Control", "max-age=600, public"
+        env.response.content_type = "text/javascript"
+        render "dist/bundle.js.ecr"
+      end
+
+      # POST requests to /posts create a new posts. No idea
+      # how long-lived this will be since we eventually want
+      # all admin stuff to go through a client-side app
+      # that talks to a JSON API.
+      #
+      post "/posts" do |env|
+        post = Post.new
+        post.title = env.params.body["post[title]"].as(String)
+        post.body =  env.params.body["post[body]"].as(String)
+
+        _changeset = Repo.insert(post)
+
+        env.redirect "/"
+      end
+
       Kemal.run
     end
   end
