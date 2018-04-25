3072d610cea191ea79517952a60d43380f8f01b8
Hendrik Mans <hendrik@mans.de>
2018-04-25 19:02:13 +0200

    WIP

--- src/crankypants/web.cr
+++ src/crankypants/web.cr
@@ -1,5 +1,6 @@
-require "kemal"
+require "kilt"
 require "kilt/slang"
+require "http/server"
 require "crecto"
 require "./formatter"
 require "./models/*"
@@ -17,30 +18,71 @@ module Crankypants
     Post = Models::Post
 
     def self.run
-      puts ["Welcome to ", "CrankyPants".colorize(:white), "! ", ":D ".colorize(:yellow), "(#{Crankypants::VERSION})".colorize(:dark_gray)].join
-      puts ["-> ".colorize(:green), "Your blog: ", "http://localhost:3000/".colorize(:cyan)].join
-
-      # We only want to mount /app and /api if the required environment
-      # variables are available.
+      # puts ["Welcome to ", "CrankyPants".colorize(:white), "! ", ":D ".colorize(:yellow), "(#{Crankypants::VERSION})".colorize(:dark_gray)].join
+      # puts ["-> ".colorize(:green), "Your blog: ", "http://localhost:3000/".colorize(:cyan)].join
       #
-      if ENV["CRANKY_LOGIN"]? && ENV["CRANKY_PASSWORD"]?
-        Api.mount
-        App.mount
-        puts ["-> ".colorize(:green), "Your app:  ", "http://localhost:3000/app/".colorize(:cyan)].join
-      else
-        puts ["-> ".colorize(:yellow), "/app".colorize(:white), " and ", "/api".colorize(:white), " are disabled. Please provide CRANKY_LOGIN and CRANKY_PASSWORD!"].join
-      end
-
-      puts ["Enjoy! ", "<3<3<3".colorize(:red)].join
-      puts
-
-      # We definitely always want to mount the public-facing blog.
+      # # We only want to mount /app and /api if the required environment
+      # # variables are available.
+      # #
+      # if ENV["CRANKY_LOGIN"]? && ENV["CRANKY_PASSWORD"]?
+      #   Api.mount
+      #   App.mount
+      #   puts ["-> ".colorize(:green), "Your app:  ", "http://localhost:3000/app/".colorize(:cyan)].join
+      # else
+      #   puts ["-> ".colorize(:yellow), "/app".colorize(:white), " and ", "/api".colorize(:white), " are disabled. Please provide CRANKY_LOGIN and CRANKY_PASSWORD!"].join
+      # end
       #
-      Blog.mount
-
-      # LET'S DO THIS!
+      # puts ["Enjoy! ", "<3<3<3".colorize(:red)].join
+      # puts
       #
-      Kemal.run
+      # # We definitely always want to mount the public-facing blog.
+      # #
+      # Blog.mount
+      #
+      # # LET'S DO THIS!
+      # #
+      # Kemal.run
+
+      puts "ready: http://localhost:3000"
+      HTTP::Server.new("127.0.0.1", 3000, [
+        HTTP::ErrorHandler.new,
+        HTTP::LogHandler.new,
+        HTTP::CompressHandler.new,
+        HTTP::StaticFileHandler.new("./public/"),
+        ApiHandler.new,
+      ]).listen
     end
+  end
+end
+
+class ApiHandler
+  include HTTP::Handler
+
+  private macro within(path)
+    if request.path =~ \%r\{\A{{ path.id }}\b}
+      {{ yield }}
+    end
+  end
+
+  private macro get(path)
+    if request.path =~ \%r\{\A{{ path.id }}/?\Z}
+      {{ yield }}
+      return
+    end
+  end
+
+  def call(context)
+    request = context.request
+    response = context.response
+    parts = request.path.split('/').reject(&.blank?)
+
+    within "/api" do
+      get "/api/posts" do
+        response.content_type = "text/html"
+        response.print "POSTS!"
+      end
+    end
+
+    call_next(context)
   end
 end
