9b1abe5bae6efb2079c0f1178b6cc511a1b7495d
Hendrik Mans <hendrik@mans.de>
2018-04-29 16:31:14 +0200

    Authentication

--- src/crankypants/web/handler.cr
+++ src/crankypants/web/handler.cr
@@ -11,6 +11,7 @@ require "./input_mappings"
 module Crankypants::Web
   class Router < Crappy::Router
     include Helpers
+    include Crappy::Authentication
 
     def call
       serve_static_assets || serve_blog || serve_api || serve_app || serve_foo
@@ -43,46 +44,46 @@ module Crankypants::Web
 
     def serve_api
       within "api" do
-        # protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"]
-
-        within "posts" do
-          get do
-            render json: Data.load_posts
-          end
-
-          post do
-            input = InputMappings::Post.from_json(request.body.not_nil!)
-
-            changeset = Data.create_post \
-              title: input.title,
-              body:  input.body
-
-            changeset.valid? ?
-              render json: changeset.instance :
-              render_json_error "Invalid post data."
-          end
-
-          within ":id" do |params|
-            post_id = params["id"].not_nil!.to_i
-
-            delete do
-              Data.delete_post(post_id)
-              render :nothing, status: 204
+        protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"] do
+          within "posts" do
+            get do
+              render json: Data.load_posts
             end
 
-            patch do
-              post  = Data.load_post(post_id)
+            post do
               input = InputMappings::Post.from_json(request.body.not_nil!)
 
-              post.title = input.title
-              post.body  = input.body
-              changeset  = Data.update_post(post)
-
-              puts "patching!"
+              changeset = Data.create_post \
+                title: input.title,
+                body:  input.body
 
               changeset.valid? ?
                 render json: changeset.instance :
                 render_json_error "Invalid post data."
+            end
+
+            within ":id" do |params|
+              post_id = params["id"].not_nil!.to_i
+
+              delete do
+                Data.delete_post(post_id)
+                render :nothing, status: 204
+              end
+
+              patch do
+                post  = Data.load_post(post_id)
+                input = InputMappings::Post.from_json(request.body.not_nil!)
+
+                post.title = input.title
+                post.body  = input.body
+                changeset  = Data.update_post(post)
+
+                puts "patching!"
+
+                changeset.valid? ?
+                  render json: changeset.instance :
+                  render_json_error "Invalid post data."
+              end
             end
           end
         end
@@ -91,8 +92,9 @@ module Crankypants::Web
 
     def serve_app
       within "app" do
-        # protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"]
-        render text: Kilt.render("src/crankypants/web/views/app.slang"), content_type: "text/html"
+        protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"] do
+          render text: Kilt.render("src/crankypants/web/views/app.slang"), content_type: "text/html"
+        end
       end
     end
 
@@ -109,29 +111,5 @@ module Crankypants::Web
     def call(context)
       Router.call(context) || call_next(context)
     end
-
-    # def call(context)
-    #   crappy do
-    #     {% if flag?(:release) %}
-    #       serve_static_assets
-    #     {% end %}
-    #
-    #     serve_blog
-    #     serve_app
-    #     serve_api
-    #   end
-    #
-    #   call_next(context)
-    # end
-
-    # private macro serve_static_assets
-    #   within "assets" do
-    #     within ":version" do
-    #       get ":filename" do
-    #         serve_static_asset "assets/#{params["filename"]}"
-    #       end
-    #     end
-    #   end
-    # end
   end
 end
--- src/crappy.cr
+++ src/crappy.cr
@@ -12,6 +12,24 @@
 #
 
 module Crappy
+  module Authentication
+    def protect_with(username, password)
+      auth_info = if request.headers["Authorization"]? && request.headers["Authorization"] =~ /^Basic (.+)$/
+        Base64.decode_string($1).split(':')
+      else
+        [nil, nil]
+      end
+
+      if [username, password] == auth_info
+        yield
+      else
+        # Authorization failed...
+        response.headers["WWW-Authenticate"] = "Basic realm=\"Crankypants!\""
+        render text: "Please log in.", status: 401
+      end
+    end
+  end
+
   class Router
     property context : HTTP::Server::Context
     property request : HTTP::Request
