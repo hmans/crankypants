d79374b4fd6d78ac8a8fd3c7cfb9583e03e791c2
Hendrik Mans <hendrik@mans.de>
2018-04-29 14:17:22 +0200

    Let's start from scratch

--- src/crankypants/web/handler.cr
+++ src/crankypants/web/handler.cr
@@ -9,98 +9,121 @@ require "./helpers"
 require "./input_mappings"
 
 module Crankypants::Web
+  class Router < Crappy::Router
+    def call
+      on :get do
+        context.response.content_type = "text/html"
+        context.response << "Home!"
+      end
+
+      on :get, "foo" do
+        context.response.content_type = "text/html"
+        context.response << "Foooooo!"
+      end
+
+      return if done?
+      context.response << "404 Not Found"
+    end
+  end
+
   class Handler
     include HTTP::Handler
-    include Crappy::Routing
-    include Crappy::Serving
-    include Crappy::Authentication
-    include Helpers
 
     def call(context)
-      crappy do
-        {% if flag?(:release) %}
-          serve_static_assets
-        {% end %}
-
-        serve_blog
-        serve_app
-        serve_api
-      end
-
-      call_next(context)
+      router = Router.new(context)
+      router.call
+      # if remaining_path.first == "foo"
+      #   return
+      # end
+      #
+      # call_next
     end
 
-    private macro serve_static_assets
-      within "assets" do
-        within ":version" do
-          get ":filename" do
-            serve_static_asset "assets/#{params["filename"]}"
-          end
-        end
-      end
-    end
+    # def call(context)
+    #   crappy do
+    #     {% if flag?(:release) %}
+    #       serve_static_assets
+    #     {% end %}
+    #
+    #     serve_blog
+    #     serve_app
+    #     serve_api
+    #   end
+    #
+    #   call_next(context)
+    # end
 
-    private macro serve_blog
-      get do
-        serve PostView.index(Data.load_posts)
-      end
-
-      within "posts" do
-        get ":id" do
-          serve PostView.show(Data.load_post(params["id"].not_nil!.to_i))
-        end
-      end
-    end
-
-    private macro serve_app
-      within "app" do
-        protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"]
-        serve template: "src/crankypants/web/views/app.slang"
-      end
-    end
-
-    private macro serve_api
-      within "api" do
-        protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"]
-
-        within "posts" do
-          get do
-            serve json: Data.load_posts
-          end
-
-          post do
-            input = InputMappings::Post.from_json(request.body.not_nil!)
-
-            changeset = Data.create_post \
-              title: input.title,
-              body:  input.body
-
-            changeset.valid? ?
-              serve json: changeset.instance :
-              serve_json_error "Invalid post data."
-          end
-
-          within ":id" do
-            delete do
-              Data.delete_post(params["id"].not_nil!.to_i)
-              serve :nothing, status: 204
-            end
-
-            patch do
-              post  = Data.load_post(params["id"].not_nil!.to_i)
-              input = InputMappings::Post.from_json(request.body.not_nil!)
-
-              post.title = input.title
-              post.body  = input.body
-              changeset  = Data.update_post(post)
-
-              changeset.valid? ?
-                serve json: changeset.instance, status: 204 :
-                serve_json_error "Invalid post data."
-            end
-          end
-        end
-      end
-    end
+    # private macro serve_static_assets
+    #   within "assets" do
+    #     within ":version" do
+    #       get ":filename" do
+    #         serve_static_asset "assets/#{params["filename"]}"
+    #       end
+    #     end
+    #   end
+    # end
+    #
+    # private macro serve_blog
+    #   get do
+    #     serve PostView.index(Data.load_posts)
+    #   end
+    #
+    #   within "posts" do
+    #     get ":id" do
+    #       serve PostView.show(Data.load_post(params["id"].not_nil!.to_i))
+    #     end
+    #   end
+    # end
+    #
+    # private macro serve_app
+    #   within "app" do
+    #     protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"]
+    #     serve template: "src/crankypants/web/views/app.slang"
+    #   end
+    # end
+    #
+    # private macro serve_api
+    #   within "api" do
+    #     protect_with ENV["CRANKY_LOGIN"], ENV["CRANKY_PASSWORD"]
+    #
+    #     within "posts" do
+    #       get do
+    #         serve json: Data.load_posts
+    #       end
+    #
+    #       post do
+    #         input = InputMappings::Post.from_json(request.body.not_nil!)
+    #
+    #         changeset = Data.create_post \
+    #           title: input.title,
+    #           body:  input.body
+    #
+    #         changeset.valid? ?
+    #           serve json: changeset.instance :
+    #           serve_json_error "Invalid post data."
+    #       end
+    #
+    #       within ":id" do
+    #         delete do
+    #           Data.delete_post(params["id"].not_nil!.to_i)
+    #           serve :nothing, status: 204
+    #         end
+    #
+    #         patch do
+    #           post  = Data.load_post(params["id"].not_nil!.to_i)
+    #           input = InputMappings::Post.from_json(request.body.not_nil!)
+    #
+    #           post.title = input.title
+    #           post.body  = input.body
+    #           changeset  = Data.update_post(post)
+    #
+    #           changeset.valid? ?
+    #             serve json: changeset.instance, status: 204 :
+    #             serve_json_error "Invalid post data."
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
   end
 end
--- src/crappy.cr
+++ src/crappy.cr
@@ -1,100 +1,167 @@
-class HTTP::Request
-  # property url_params : Hash(String, String)?
-end
+# class HTTP::Request
+#   # property url_params : Hash(String, String)?
+# end
+#
+# class HTTP::Server::Context
+#   def remaining_path
+#     @_remaining_path ||= begin
+#       request.path.split('/').reject(&.blank?)
+#     end
+#   end
+# end
+#
 
 module Crappy
-  module Routing
-    private macro crappy
-      request = context.request
-      response = context.response
-      parts = request.path.split('/').reject(&.blank?)
+  class Router
+    property context : HTTP::Server::Context
+    property request : HTTP::Request
+    property response : HTTP::Server::Response
+    property remaining_parts : Array(String)
 
-      {{ yield }}
+    def initialize(@context)
+      @request = @context.request
+      @response = @context.response
+      @remaining_parts = @request.path.split('/').reject(&.blank?)
+      @done = false
     end
 
-    private macro part_to_regex(part)
-      Regex.new("\\A" + {{ part }}.gsub(/:(\w+)/, "(?<\\1>.+)") + "\\Z")
+    def call
+      raise "Implement me"
     end
 
-    private macro within(part = nil)
-      {% if part %}
-        if parts.any? && (md = part_to_regex({{ part }}).match(parts[0]))
-          # Extract named captures into params... for now
-          params = md.named_captures
-
-          # With the next path part removed, execute the given block
-          buffer = parts.shift
-          {{ yield }}
-          parts.unshift buffer
-        end
-      {% else %}
-        {{ yield }}
-      {% end %}
-    end
-
-    private macro on(method, part)
-      if request.method == "{{ method.id.upcase }}"
-        within {{ part }} do
-          # Only execute the given block when no further parts are available.
-          if parts.empty?
-            # Execute the given block and return, preventing remaining
-            # crappy code to tun.
-            {{ yield }}
-            return
+    private def on(method : Symbol, part : String | Nil = nil)
+      return if done?
+      if request.method == method.to_s.upcase
+        within part do
+          if remaining_parts.empty?
+            yield
+            halt
           end
         end
       end
     end
 
-    {% for method in [:get, :put, :post, :patch, :delete] %}
-      private macro {{ method.id }}(part = nil)
-        on {{ method }}, \{{ part }} { \{{ yield }} }   # I'm not kidding
-      end
-    {% end %}
-  end
-
-  module Serving
-    private macro serve(output = nil, status = 200, content_type = "text/html", template = nil, json = nil)
-      # Set response status code
-      {% if status %}
-        response.status_code = {{ status }}
-      {% end %}
-
-      # Set response content type
-      {% if content_type %}
-        response.content_type = {{ content_type }}
-      {% end %}
-
-      # Render response body
-      {% if template %}
-        response.print(Kilt.render({{ template }}))
-      {% elsif json %}
-        response.content_type = "application/json"
-        response.print({{ json }}.to_json)
-      {% elsif output == :nothing %}
-        # nothing!
-      {% elsif output %}
-        response.print({{ output }})
-      {% end %}
-
-      # Always return, ending request processing.
-      return
+    private def within(part : Nil)
+      return if done?
+      yield
     end
-  end
 
-  module Authentication
-    private macro protect_with(username, password)
-      auth_info = if request.headers["Authorization"]? && request.headers["Authorization"] =~ /^Basic (.+)$/
-        Base64.decode_string($1).split(':')
-      else
-        [nil, nil]
+    private def within(part : String)
+      return if done?
+      if next_part_matches?(part)
+        buffer = remaining_parts.shift
+        yield
+        remaining_parts.unshift(buffer)
       end
+    end
 
-      if [{{username}}, {{password}}] != auth_info
-        # Authorization failed...
-        response.headers["WWW-Authenticate"] = "Basic realm=\"Crankypants!\""
-        serve "Please log in.", status: 401
-      end
+    private def next_part_matches?(part)
+      remaining_parts.any? && remaining_parts.first == part
+    end
+
+    private def halt
+      @done = true
+    end
+
+    def done?
+      @done
     end
   end
 end
+
+# module Crappy
+#   module Routing
+#     private macro crappy
+#       request = context.request
+#       response = context.response
+#       parts = request.path.split('/').reject(&.blank?)
+#
+#       {{ yield }}
+#     end
+#
+#     private macro part_to_regex(part)
+#       Regex.new("\\A" + {{ part }}.gsub(/:(\w+)/, "(?<\\1>.+)") + "\\Z")
+#     end
+#
+#     private macro within(part = nil)
+#       {% if part %}
+#         if parts.any? && (md = part_to_regex({{ part }}).match(parts[0]))
+#           # Extract named captures into params... for now
+#           params = md.named_captures
+#
+#           # With the next path part removed, execute the given block
+#           buffer = parts.shift
+#           {{ yield }}
+#           parts.unshift buffer
+#         end
+#       {% else %}
+#         {{ yield }}
+#       {% end %}
+#     end
+#
+#     private macro on(method, part)
+#       if request.method == "{{ method.id.upcase }}"
+#         within {{ part }} do
+#           # Only execute the given block when no further parts are available.
+#           if parts.empty?
+#             # Execute the given block and return, preventing remaining
+#             # crappy code to tun.
+#             {{ yield }}
+#             return
+#           end
+#         end
+#       end
+#     end
+#
+#     {% for method in [:get, :put, :post, :patch, :delete] %}
+#       private macro {{ method.id }}(part = nil)
+#         on {{ method }}, \{{ part }} { \{{ yield }} }   # I'm not kidding
+#       end
+#     {% end %}
+#   end
+#
+#   module Serving
+#     private macro serve(output = nil, status = 200, content_type = "text/html", template = nil, json = nil)
+#       # Set response status code
+#       {% if status %}
+#         response.status_code = {{ status }}
+#       {% end %}
+#
+#       # Set response content type
+#       {% if content_type %}
+#         response.content_type = {{ content_type }}
+#       {% end %}
+#
+#       # Render response body
+#       {% if template %}
+#         response.print(Kilt.render({{ template }}))
+#       {% elsif json %}
+#         response.content_type = "application/json"
+#         response.print({{ json }}.to_json)
+#       {% elsif output == :nothing %}
+#         # nothing!
+#       {% elsif output %}
+#         response.print({{ output }})
+#       {% end %}
+#
+#       # Always return, ending request processing.
+#       return
+#     end
+#   end
+#
+#   module Authentication
+#     private macro protect_with(username, password)
+#       auth_info = if request.headers["Authorization"]? && request.headers["Authorization"] =~ /^Basic (.+)$/
+#         Base64.decode_string($1).split(':')
+#       else
+#         [nil, nil]
+#       end
+#
+#       if [{{username}}, {{password}}] != auth_info
+#         # Authorization failed...
+#         response.headers["WWW-Authenticate"] = "Basic realm=\"Crankypants!\""
+#         serve "Please log in.", status: 401
+#       end
+#     end
+#   end
+# end
