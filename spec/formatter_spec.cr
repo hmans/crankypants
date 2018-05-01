71155d259e1ab060f4b962c3bf503936f1dfc0dd
Hendrik Mans <hendrik@mans.de>
2018-05-01 19:48:03 +0200

    Add pending specs for autolinking (#16)

--- spec/formatter_spec.cr
+++ spec/formatter_spec.cr
@@ -20,4 +20,21 @@ describe Formatter do
     format("<pre>**yay**</pre>")
       .should eq("<pre>**yay**</pre>\n")
   end
+
+  pending "autolinking" do
+    it "autolinks URLs" do
+      format("My blog is at http://hmans.io!")
+        .should eq("<p>My blog is at <a href=\"http://hmans.io\">http://hmans.io</a>!</p>\n")
+    end
+
+    it "does not change URLs in tag attributes" do
+      format("<img src=\"http://hmans.io/kitten.jpg\">")
+        .should eq("<img src=\"http://hmans.io/kitten.jpg\">\n")
+    end
+
+    it "does not change URLs in <pre> blocks" do
+      format("<pre><code>http://hmans.io</code></pre>")
+        .should eq("<pre><code>http://hmans.io</code></pre>\n")
+    end
+  end
 end
