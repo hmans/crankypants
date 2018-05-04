0746f9d6a2d0b152d766eed62fa95305928670b3
Hendrik Mans <hendrik@mans.de>
2018-05-04 10:25:04 +0200

    Minor cleanup

--- e2e-tests/spec/spec_helper.rb
+++ e2e-tests/spec/spec_helper.rb
@@ -2,10 +2,12 @@ require "rspec"
 require "capybara/rspec"
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :remote,
-    url: "http://selenium:4444/wd/hub", desired_capabilities: :chrome)
+  Capybara::Selenium::Driver.new app,
+    browser: :remote,
+    url: "http://selenium:4444/wd/hub",
+    desired_capabilities: :chrome
 end
 
-Capybara.default_driver = :chrome
+Capybara.default_driver    = :chrome
 Capybara.javascript_driver = :chrome
-Capybara.app_host = "http://crankypants:3000"
+Capybara.app_host          = "http://crankypants:3000"
