b84602b80df837e5f4e1ac5b94060c9b149fae3c
Hendrik Mans <hendrik@mans.de>
2018-05-05 13:08:45 +0200

    Test Bob, too!

--- e2e-tests/spec/creating_a_post_spec.rb
+++ e2e-tests/spec/creating_a_post_spec.rb
@@ -2,12 +2,19 @@ require "spec_helper"
 
 describe "creating a post", type: :feature do
   specify do
-    visit "/app"
+    on :alice do
+      visit "/app"
 
-    fill_in "post_body", with: "Hello world"
-    click_on "Save Post"
+      fill_in "post_body", with: "Hello world"
+      click_on "Save Post"
 
-    visit "/"
-    expect(page).to have_css(:article, text: "Hello world")
+      visit "/"
+      expect(page).to have_css(:article, text: "Hello world")
+    end
+
+    on :bob do
+      visit "/"
+      expect(page).to have_text("And I Am Bob")
+    end
   end
 end
--- e2e-tests/spec/spec_helper.rb
+++ e2e-tests/spec/spec_helper.rb
@@ -11,3 +11,11 @@ end
 Capybara.default_driver    = :chrome
 Capybara.javascript_driver = :chrome
 Capybara.app_host          = "http://alice:secret@alice:3000"
+
+def on(who)
+  previous_host = Capybara.app_host
+  # default_url_options[:host] = host
+  Capybara.app_host = "http://#{who}:secret@#{who}:3000"
+  yield
+  Capybara.app_host = previous_host
+end
