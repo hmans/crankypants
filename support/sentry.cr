e9365c022ea341a0d6dbeeea8a48e6c9ee1776a3
Hendrik Mans <hendrik@mans.de>
2018-05-25 17:43:18 +0200

    Use webpack-dev-server with hot reloading for development (#29)

--- support/sentry.cr
+++ support/sentry.cr
@@ -4,6 +4,7 @@ sentry = Sentry::ProcessRunner.new(
   process_name: "Crankypants DEV",
   build_command: "crystal build ./src/crankypants_cli.cr -o ./crankypants",
   run_command: "./crankypants",
+  run_args: ["--asset-host", "localhost:8080"],
   files: ["./src/**/*.cr", "./src/**/*.ecr", "./src/**/*.slang"]
 )
 
