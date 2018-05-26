d8f00a3b62aee11e8c84c3f39a66d195bdd9f287
Hendrik Mans <hendrik@mans.de>
2018-05-26 21:39:05 +0200

    Massively speed up compilation in development
    ...by compiling into bin/ and mounting that as a non-external volume. No
    more copying a 1 MB executable over to the host OS!

--- support/sentry.cr
+++ support/sentry.cr
@@ -2,8 +2,8 @@ require "sentry"
 
 sentry = Sentry::ProcessRunner.new(
   process_name: "Crankypants DEV",
-  build_command: "crystal build ./src/crankypants_cli.cr -o ./crankypants",
-  run_command: "./crankypants",
+  build_command: "crystal build ./src/crankypants_cli.cr -o bin/crankypants",
+  run_command: "./bin/crankypants",
   run_args: ["--asset-host", "localhost:8080"],
   files: ["./src/**/*.cr", "./src/**/*.ecr", "./src/**/*.slang"]
 )
