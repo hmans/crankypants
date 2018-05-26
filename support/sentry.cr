8398cc3bd090d8f85fb7269efc085ee7f0133d1b
Hendrik Mans <hendrik@mans.de>
2018-05-26 22:06:07 +0200

    Change Sentry configuration to also look for changes in relocated Crappy files

--- support/sentry.cr
+++ support/sentry.cr
@@ -5,7 +5,7 @@ sentry = Sentry::ProcessRunner.new(
   build_command: "crystal build ./src/crankypants_cli.cr -o bin/crankypants",
   run_command: "./bin/crankypants",
   run_args: ["--asset-host", "localhost:8080"],
-  files: ["./src/**/*.cr", "./src/**/*.ecr", "./src/**/*.slang"]
+  files: ["./src/**/*", "./crappy/src/**/*"]
 )
 
 sentry.run
