775bfe33adbc36b7175591fa111852b25ceb6c4d
Hendrik Mans <hendrik@mans.de>
2018-06-18 15:11:01 +0200

    Update to Crystal 0.25.0 (#35)
    * Change Dockerfiles to use 0.25.0
    * Fix HTTP::Server instantiation
    * Monkeypatch Sentry
    * Update dependencies

--- support/sentry.cr
+++ support/sentry.cr
@@ -1,5 +1,12 @@
 require "sentry"
 
+# Monkeypatch for 0.25 compatibility
+class Sentry::ProcessRunner
+  private def get_timestamp(file : String)
+    File.info(file).modification_time.to_s("%Y%m%d%H%M%S")
+  end
+end
+
 sentry = Sentry::ProcessRunner.new(
   process_name: "Crankypants DEV",
   build_command: "crystal build ./src/crankypants_cli.cr -o bin/crankypants",
