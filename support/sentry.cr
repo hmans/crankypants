61bfe3e7f6a7834a7efeebc0a1563e7308283100
Hendrik Mans <hendrik@mans.de>
2018-06-22 22:50:29 +0200

    Remove obsolete code

--- support/sentry.cr
+++ support/sentry.cr
@@ -1,12 +1,5 @@
 require "sentry"
 
-# Monkeypatch for 0.25 compatibility
-# class Sentry::ProcessRunner
-#   private def get_timestamp(file : String)
-#     File.info(file).modification_time.to_s("%Y%m%d%H%M%S")
-#   end
-# end
-
 sentry = Sentry::ProcessRunner.new(
   display_name: "Crankypants DEV",
   build_command: "crystal build ./src/crankypants_cli.cr -o bin/crankypants",
