4c2e1a167e247fb7f83ec8f506d1f5f15c5ed4db
Hendrik Mans <hendrik@mans.de>
2018-06-22 22:49:21 +0200

    Use my own fork of sentry because that's apparently the only way to fix this. I miss Rust.

--- support/sentry.cr
+++ support/sentry.cr
@@ -1,14 +1,14 @@
 require "sentry"
 
 # Monkeypatch for 0.25 compatibility
-class Sentry::ProcessRunner
-  private def get_timestamp(file : String)
-    File.info(file).modification_time.to_s("%Y%m%d%H%M%S")
-  end
-end
+# class Sentry::ProcessRunner
+#   private def get_timestamp(file : String)
+#     File.info(file).modification_time.to_s("%Y%m%d%H%M%S")
+#   end
+# end
 
 sentry = Sentry::ProcessRunner.new(
-  process_name: "Crankypants DEV",
+  display_name: "Crankypants DEV",
   build_command: "crystal build ./src/crankypants_cli.cr -o bin/crankypants",
   run_command: "./bin/crankypants",
   run_args: ["--asset-host", "localhost:8080"],
